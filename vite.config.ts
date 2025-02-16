40df553b3834373529e89cc17c2b4d712b648c41
mfuadfakhruzzaki <mfuadfakhruzzaki@students.undip.ac.id>
2025-02-16 08:50:45 +0700

    Update index.html and vite.config.ts for PWA support and improve app metadata

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
+// vite.config.ts
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      registerType: "autoUpdate",
+      // File-file statis yang akan di-copy ke folder build
+      includeAssets: [
+        "favicon.ico",
+        "AppIcon@3x.png", // iOS icon (180x180)
+        "icons/icon-192x192.png", // Android/Chrome
+        "icons/icon-512x512.png", // Android/Chrome
+      ],
       manifest: {
         name: "Eatgorithm",
         short_name: "Eatgorithm",
@@ -17,18 +25,17 @@ export default defineConfig({
         theme_color: "#3f51b5",
         icons: [
           {
-            src: "public/icons/icon-192x192.png",
+            src: "icons/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "public/icons/icon-512x512.png",
+            src: "icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
         ],
       },
-      registerType: "autoUpdate",
     }),
   ],
   resolve: {
