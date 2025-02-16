ab243231074f7cd1f88c5041e8a7708dfd289288
mfuadfakhruzzaki <mfuadfakhruzzaki@students.undip.ac.id>
2025-02-16 08:43:58 +0700

    Update icon paths in Vite configuration to use public directory

--- vite.config.ts
+++ vite.config.ts
@@ -17,12 +17,12 @@ export default defineConfig({
         theme_color: "#3f51b5",
         icons: [
           {
-            src: "icons/icon-192x192.png",
+            src: "public/icons/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "icons/icon-512x512.png",
+            src: "public/icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
